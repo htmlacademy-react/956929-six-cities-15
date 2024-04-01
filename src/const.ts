5c8e9f425488da5d1390f6e24746d8cd0f33c0ae
denis.makarov <chaspik16@gmail.com>
2024-04-01 20:44:14 +0300

    6.16. Контроль и ограничения (часть 2)

--- src/const.ts
+++ src/const.ts
@@ -73,3 +73,10 @@ export const citiesList = Object.values(LOCATIONS);
 export const DEFAULT_CITY = LOCATIONS.Paris;
 
 export const [defaultLocation] = cityMap.filter((item) => item.title === DEFAULT_CITY);
+
+export enum SortType {
+  Popular = 'Popular',
+  LowToHigh = 'Price: low to high',
+  HighToLow = 'Price: high to low',
+  TopRated = 'Top rated first',
+}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,18 @@
+import { Offer } from '../types/offer';
+import { SortType } from '../const';
+
+
 export const countStars = (width: number) => `${width * 20}%`;
+
+export function sortOffers(sortType: SortType, offers: Offer[]) {
+  switch (sortType) {
+    case SortType.LowToHigh:
+      return offers.sort((a, b) => a.price - b.price);
+    case SortType.HighToLow:
+      return offers.sort((a, b) => b.price - a.price);
+    case SortType.TopRated:
+      return offers.sort((a, b) => b.rating - a.rating);
+    default:
+      return offers;
+  }
+}
